e1138b7684055bf7550e62565e2e559ae5940b99
Luis Vicente <langel.vicente@gmail.com>
2018-09-21 12:22:34 +0100

    Fix user group

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ USRID=${USER_ID:-9001}
 GRPID=${GROUP_ID:-9001}
 
 getent group ${GRPID} 2>&1 > /dev/null || groupadd -g ${GRPID} sbt
-useradd --shell /bin/bash -u ${USRID} -o -c "sbt user" -m sbt
+useradd --shell /bin/bash -u ${USRID} -g ${GRPID} -o -c "sbt user" -m sbt
 export HOME=/home/sbt
 
 chown -R ${USRID}:${GRPID} $HOME
